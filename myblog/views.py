883f138a2e9c8ca0a5eacfae83806f902a670182
zane <zane@mot.com>
2019-11-12 15:03:10 +0800

    2019.11.12 include homepage

--- myblog/views.py
+++ myblog/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, render_to_response
+from django.http import HttpResponse, HttpResponseRedirect
 # Create your views here.
+
+def homepage(request):
+    
+       
+   
+   return render(request, 'mainsite/homepage.html', locals())
